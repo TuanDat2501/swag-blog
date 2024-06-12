c0faed5aa3f3b09ef052bf60ca14e2c6c22bceb1
TuanDat2501 <tuandat2514.work@gmail.com>
2024-06-12 10:27:46 +0700

    fix UI

--- src/app/component/header/Header.tsx
+++ src/app/component/header/Header.tsx
@@ -1,13 +1,19 @@
 'use client'
-import React, {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.scss';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import IVideo from "@/icon/IVideo";
-import {usePathname, useRouter} from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import Image from 'next/image';
+import { useMediaQuery } from 'usehooks-ts';
 const Header = () => {
     const router = useRouter();
     const pathName = usePathname();
+    const isMobile = useMediaQuery('max-width:430px')
+    const [isOpen, setIsOpen] = useState(false)
+    const openMenu = () => {
+        setIsOpen(!isOpen);
+    }
 
     return (
         <div>
@@ -24,33 +30,50 @@ const Header = () => {
                         <div className="col-5 col-md-8 ml-auto mr-0">
                             <div className="tm-site-nav">
                                 <nav className="navbar navbar-expand-lg mr-0 ml-auto" id="tm-main-nav">
-                                    <button
-                                        className="navbar-toggler tm-bg-black py-2 px-3 mr-0 ml-auto collapsed"
+                                    {!isMobile ? <> <button
+                                        onClick={openMenu}
+                                        className="navbar-toggler tm-bg-black py-2 px-3 mr-0 ml-auto collapsed flex justify-center items-center"
                                         type="button"
                                         data-toggle="collapse" data-target="#navbar-nav"
                                         aria-controls="navbar-nav"
                                         aria-expanded="false" aria-label="Toggle navigation">
                                         <span>
-                                            <i className="fas fa-bars tm-menu-closed-icon"></i>
-                                            <i className="fas fa-times tm-menu-opened-icon"></i>
+
+                                            <svg viewBox="0 0 24 24" fill="none" width={30} height={30} xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 6H20M4 12H20M4 18H20" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
                                         </span>
                                     </button>
-                                    <div className="navbar-collapse tm-nav" id="navbar-nav">
-                                        <ul className="navbar-nav text-uppercase">
-                                            <li className={pathName === "/" ? "nav-item active" : "nav-item"} onClick={()=>router.push('/')}>
-                                                <a className="nav-link tm-nav-link">Videos</a>
-                                            </li>
-                                            <li className={pathName === "/submit-videos" ? "nav-item active" : "nav-item"} onClick={()=>router.push('/submit-videos')}>
-                                                <a className="nav-link tm-nav-link">Submit video</a>
-                                            </li>
-                                            <li className={pathName === "/blog" ? "nav-item active" : "nav-item"}  onClick={()=>router.push('/blog')}>
-                                                <a className="nav-link tm-nav-link">Blog</a>
-                                            </li>
-                                            <li className={pathName === "/contact-us" ? "nav-item active" : "nav-item"}  onClick={()=>router.push('/blog')}>
-                                                <a className="nav-link tm-nav-link">Contact Us</a>
-                                            </li>
-                                        </ul>
-                                    </div>
+                                        {isOpen && <div className="collapsed navbar-collapse tm-nav" id="navbar-nav">
+                                            <ul className="navbar-nav text-uppercase">
+                                                <li className={pathName === "/" ? "nav-item active" : "nav-item"} onClick={() => router.push('/')}>
+                                                    <a className="nav-link tm-nav-link">Videos</a>
+                                                </li>
+                                                <li className={pathName === "/submit-videos" ? "nav-item active" : "nav-item"} onClick={() => router.push('/submit-videos')}>
+                                                    <a className="nav-link tm-nav-link">Submit video</a>
+                                                </li>
+                                                <li className={pathName === "/blog" ? "nav-item active" : "nav-item"} onClick={() => router.push('/blog')}>
+                                                    <a className="nav-link tm-nav-link">Blog</a>
+                                                </li>
+                                                <li className={pathName === "/contact-us" ? "nav-item active" : "nav-item"} onClick={() => router.push('/contact-us')}>
+                                                    <a className="nav-link tm-nav-link">Contact Us</a>
+                                                </li>
+                                            </ul>
+                                        </div>}</> :
+                                        <div className="collapsed navbar-collapse tm-nav" id="navbar-nav">
+                                            <ul className="navbar-nav text-uppercase">
+                                                <li className={pathName === "/" ? "nav-item active" : "nav-item"} onClick={() => router.push('/')}>
+                                                    <a className="nav-link tm-nav-link">Videos</a>
+                                                </li>
+                                                <li className={pathName === "/submit-videos" ? "nav-item active" : "nav-item"} onClick={() => router.push('/submit-videos')}>
+                                                    <a className="nav-link tm-nav-link">Submit video</a>
+                                                </li>
+                                                <li className={pathName === "/blog" ? "nav-item active" : "nav-item"} onClick={() => router.push('/blog')}>
+                                                    <a className="nav-link tm-nav-link">Blog</a>
+                                                </li>
+                                                <li className={pathName === "/contact-us" ? "nav-item active" : "nav-item"} onClick={() => router.push('/contact-us')}>
+                                                    <a className="nav-link tm-nav-link">Contact Us</a>
+                                                </li>
+                                            </ul>
+                                        </div>}
                                 </nav>
                             </div>
                         </div>
